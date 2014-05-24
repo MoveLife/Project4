71c8314d0ac0a2466fe2c5d379fc27c483cc64ef
ArmindoMaurits <a.r.maurits@gmail.com>
2014-05-24 18:08:27 +0200

    Changed layout of the buttons on the menu

--- app/src/main/java/example/myapplication/movelife/Splash.java
+++ app/src/main/java/example/myapplication/movelife/Splash.java
@@ -13,13 +13,14 @@ public class Splash extends Activity {
         Thread timer = new Thread() {
             public void run(){
                 try{
-                    sleep(2000);
+                    sleep(3000);
 
             }catch (InterruptedException e){
                     e.printStackTrace();
                 }finally {
                     Intent openStartingPoint = new Intent("example.myapplication.movelife.MENU");
                     startActivity(openStartingPoint);
+                    finish();
                 }
                 }
 
