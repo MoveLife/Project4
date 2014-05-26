4cde26e6153a6496aa8c3a47350de86f06535e8e
ArmindoMaurits <a.r.maurits@gmail.com>
2014-05-26 11:21:36 +0200

    Deleted MainActivity, added icons for the Menu

--- app/src/main/java/example/myapplication/movelife/Menu.java
+++ app/src/main/java/example/myapplication/movelife/Menu.java
@@ -1,46 +1,13 @@
 package example.myapplication.movelife;
 
-import android.app.ListActivity;
-import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-/**
- * Created by Thomas on 15-5-2014.
- */
-public class Menu extends ListActivity {
-
-    String classes[] = {"MainActivity", "Example1", "example2", "example3", "example4"};
+public class Menu extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setListAdapter(new ArrayAdapter<String>(Menu.this, android.R.layout.simple_list_item_1, classes));
+        setContentView(R.layout.menu);
     }
-
-
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        String cheese = classes[position];
-        super.onListItemClick(l, v, position, id);
-        try {
-            Class ourClass = Class.forName("example.myapplication.movelife." + cheese);
-            Intent ourIntent = new Intent(Menu.this, ourClass);
-
-            startActivity(ourIntent);
-        }catch (ClassNotFoundException e){
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(android.view.Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
 }
