e797d7f2708331dffca31bd779eb03ee83bbeab4
ArmindoMaurits <a.r.maurits@gmail.com>
2014-05-26 14:11:17 +0200

    Login screen created. All intents to other activities from the menu screen are working.

---
+++ app/src/main/java/example/myapplication/movelife/Map.java
@@ -0,0 +1,13 @@
+package example.myapplication.movelife;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Map extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map);
+    }
+}
--- app/src/main/java/example/myapplication/movelife/Menu.java
+++ app/src/main/java/example/myapplication/movelife/Menu.java
@@ -1,7 +1,9 @@
 package example.myapplication.movelife;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends Activity {
 
@@ -9,5 +11,30 @@ public class Menu extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+    }
+
+    public void act_Login (View view) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+    public void act_Map (View view) {
+        Intent intent = new Intent(this, Map.class);
+        startActivity(intent);
+    }
+    public void act_Status (View view) {
+        Intent intent = new Intent(this, Status.class);
+        startActivity(intent);
+    }
+    public void act_Categories (View view) {
+        Intent intent = new Intent(this, Categories.class);
+        startActivity(intent);
+    }
+    public void act_Friends (View view) {
+        Intent intent = new Intent(this, Friends.class);
+        startActivity(intent);
+    }
+    public void act_Events (View view) {
+        Intent intent = new Intent(this, Events.class);
+        startActivity(intent);
     }
 }
