ec248b03789ef615e0f841dd9e7343e0fd2d506a
Thomas Maurer <0876337@hr.nl>
2014-06-04 20:17:48 +0200

    CustomAdapter aangemaakt

---
+++ app/src/main/java/example/myapplication/movelife/CustomBaseAdapter.java
@@ -0,0 +1,71 @@
+package example.myapplication.movelife;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomBaseAdapter extends BaseAdapter {
+    Context context;
+    List<Company> lijst = new ArrayList<Company>();
+
+    public CustomBaseAdapter(Context context, List<Company> items) {
+        this.context = context;
+        this.lijst = items;
+    }
+
+    /*private view holder class*/
+    private class ViewHolder {
+        ImageView imageView;
+        TextView txtTitle;
+        TextView txtDesc;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+
+        LayoutInflater mInflater = (LayoutInflater)
+                context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.bedrijf_info, null);
+            holder = new ViewHolder();
+            holder.txtDesc = (TextView) convertView.findViewById(R.id.Bedrijfdesc);
+            holder.txtTitle = (TextView) convertView.findViewById(R.id.Bedrijfsnaam);
+            holder.imageView = (ImageView) convertView.findViewById(R.id.imageView1);
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        Company company = (Company) getItem(position);
+
+        holder.txtDesc.setText(company.getDescription());
+        holder.txtTitle.setText(company.getName());
+        holder.imageView.setImageResource(R.id.imageView1);
+
+        return convertView;
+    }
+
+    @Override
+    public int getCount() {
+        return lijst.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return lijst.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return lijst.indexOf(getItem(position));
+    }
+}
--- app/src/main/java/example/myapplication/movelife/Friends.java
+++ app/src/main/java/example/myapplication/movelife/Friends.java
@@ -10,87 +10,34 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewManager;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Friends extends Activity {
+public  class Friends extends Activity {
 
 
-
+    ListView listView;
+    List<Company> lijst = new ArrayList<Company>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bedrijflist);
-        LayoutInflater li = getLayoutInflater();
-        View v = li.inflate(R.layout.bedrijflist,null);
 
-        ListView codeLearnLessons = (ListView)v.findViewById(R.id.lijst1);
-
-        BedrijfCustomAdapter listAdapter = new BedrijfCustomAdapter(this);
-        codeLearnLessons.setAdapter(listAdapter);
-
+        listView = (ListView) findViewById(R.id.lijst1);
+        CustomBaseAdapter adapter = new CustomBaseAdapter(this, lijst);
+        listView.setAdapter(adapter);
 
     }
+
 }
 
-class BedrijfCustomAdapter extends BaseAdapter {
-
-    Company company = new Company(0, "Jan", 0, 0);
-    List<Company> lijst = new ArrayList<Company>();
-
-    private final Context context;
-
-    public BedrijfCustomAdapter(Context context) {
-        this.context = context;
-
-    }
 
 
-    @Override
-    public int getCount() {
-        return lijst.size();
-    }
 
-    @Override
-    public Object getItem(int position) {
-        return lijst.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(int position, View view, ViewGroup parent) {
-
-
-        if(view==null)
-        {
-
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            view = inflater.inflate(R.layout.bedrijf_info, parent,false);
-        }
-
-        TextView bedrijfsnaam = (TextView)view.findViewById(R.id.Bedrijfsnaam);
-        TextView bedrijfdesc = (TextView)view.findViewById(R.id.Bedrijfdesc);
-
-
-        Company company = lijst.get(position); //lijst[position]
-        Log.d("lolz",company.toString());
-        bedrijfsnaam.setText(company.getName());
-        if(company.getDescription() == null) {
-            bedrijfdesc.setText(".");
-        } else {
-            bedrijfdesc.setText(company.getDescription());
-        }
-        return view;
-    }
-
-
-}
 
 
