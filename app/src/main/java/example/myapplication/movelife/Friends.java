846dbfd4dae6f58de608ba883e94aa7dff012e57
Thomas Maurer <0876337@hr.nl>
2014-06-11 15:11:01 +0200

    Libraries

--- app/src/main/java/example/myapplication/movelife/Friends.java
+++ app/src/main/java/example/myapplication/movelife/Friends.java
@@ -31,30 +31,29 @@ public  class Friends extends Activity {
         setContentView(R.layout.bedrijflist);
         LocalDatabaseConnector.init(this);
         List<Company> lijst = Company.getCompanies();
-        cv.put("bid", 1);
-        cv.put("name", "Mijnbedrijf" );
-        cv.put("latitude", 1.1);
-        cv.put("longitude", 1.2);
-        cv.put("cid", 1);
-        cv.put("postcode", 3124);
-        cv.put("address", 12);
-        cv.put("rating", 1.1);
-        cv.put("telephone", 321);
-        cv.put("description", "Goed");
-        cv.put("buystate", 1);
+       // cv.put("bid", 1);
+      //  cv.put("name", "Mijnbedrijf" );
+       // cv.put("latitude", 1.1);
+       // cv.put("longitude", 1.2);
+       // cv.put("cid", 1);
+      //  cv.put("postcode", 3124);
+      //  cv.put("address", 12);
+      //  cv.put("rating", 1.1);
+       // cv.put("telephone", 321);
+      //  cv.put("description", "Goed");
+      //  cv.put("buystate", 1);
 
 
-        LocalDatabaseConnector.insert("companies", null, cv);
+    //    LocalDatabaseConnector.insert("companies", null, cv);
         Log.d("lijst", lijst.toString());
-        Log.d("lol",""+ LocalDatabaseConnector.insert("companies", null, cv));
+      //  Log.d("lol",""+ LocalDatabaseConnector.insert("companies", null, cv));
         Log.d("companysize", ""+ Company.getCompanies().size());
 
         listView = (ListView) findViewById(R.id.lijst1);
-        CustomBaseAdapter adapter = new CustomBaseAdapter(this, lijst);
+        CustomBaseAdapterAlleBedrijven adapter = new CustomBaseAdapterAlleBedrijven(this, lijst);
         listView.setAdapter(adapter);
 
     }
-
 
 
 
