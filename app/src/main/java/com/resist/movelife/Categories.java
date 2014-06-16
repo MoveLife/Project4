61dfde6a93480cb5ee22fb0cece605d07126d15d
ArmindoMaurits <a.r.maurits@gmail.com>
2014-06-16 17:03:09 +0200

    Update Categories.java
    animatie fix

--- app/src/main/java/com/resist/movelife/Categories.java
+++ app/src/main/java/com/resist/movelife/Categories.java
@@ -3,12 +3,11 @@ package com.resist.movelife;
 import android.app.Activity;
 import android.os.Bundle;
 
-
-
 public class Categories extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.categories);
+        overridePendingTransition(R.anim.anim_in, R.anim.anim_out);
     }
 }
