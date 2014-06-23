98e7f568e0ce8f169f3ae118f498366172a03078
ArmindoMaurits <a.r.maurits@gmail.com>
2014-06-23 15:43:49 +0200

    Button en current emailadress verborgen van change email. Connectie naar menu gemaakt in de login voor 't opvragen van een ingesteld email en password.

--- app/src/main/java/com/resist/movelife/AccountSettings.java
+++ app/src/main/java/com/resist/movelife/AccountSettings.java
@@ -4,14 +4,20 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class AccountSettings extends Activity{
+    String currentEmail = Menu.currentEmail();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accountsettings);
         overridePendingTransition(R.anim.anim_in, R.anim.anim_out);
+
+        TextView CurrentEmail = (TextView)findViewById(R.id.tv_CurrentEmail);
+        CurrentEmail.setText(currentEmail);
     }
 
     public void act_changeEmail (View view) {
