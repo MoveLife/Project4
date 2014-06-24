82a8f981beb939d12924c8134d23bda76ce49bde
ArmindoMaurits <a.r.maurits@gmail.com>
2014-06-24 14:33:02 +0200

    Changemail fixed met updaten naar de server. Werkend. Moet nog gedaan worden voor changepassword.

--- app/src/main/java/com/resist/movelife/AccountSettings.java
+++ app/src/main/java/com/resist/movelife/AccountSettings.java
@@ -15,7 +15,7 @@ public class AccountSettings extends Activity{
         overridePendingTransition(R.anim.anim_in, R.anim.anim_out);
 
         TextView CurrentEmail = (TextView)findViewById(R.id.tv_CurrentEmail);
-        CurrentEmail.setText(Menu.currentEmail());
+        CurrentEmail.setText(Menu.getUpdater().getEmail());
     }
 
     public void act_changeEmail (View view) {
